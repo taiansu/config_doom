8260b35236318436853352bf5ac4786b4ff9df52
Tai An Su <taiansu@gmail.com>
2019-03-20 02:15:58 +0800

    Let's start from here

---
+++ packages.el
@@ -0,0 +1,5 @@
+;; ~/.doom.d/packages.el (example)
+
+;; This will also instruct Doom to ignore def-package! blocks for this package
+(package! alchemist :disable t)
+(package! alchemist-ac :disable t)
